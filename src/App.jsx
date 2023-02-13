35d97d4ce779c8c7478ea6217a518313ae6f1884
Marcelo Moreira <mmoreira@fivecom.com.br>
2023-02-13 17:27:49 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {Container, HeaderContainer, HeaderH1} from "./assets/Styles.js";
+
+function App() {
+
+    return (
+    <div className="App">
+        <Container>
+            <HeaderContainer>
+                <HeaderH1>
+                    Login
+                </HeaderH1>
+            </HeaderContainer>
+        </Container>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/Styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+  width: 30rem;
+  height: 35rem;
+  max-height: 100vh;
+  background-color: rgba(255, 78, 78, 0.82);
+  border-radius: 10px;
+  box-shadow: 8px 5px 5px #361010;
+  display: flex;
+  justify-content: center;
+`;
+
+export const HeaderContainer = styled.div`
+  width: 90%;
+  height: 30%;
+  margin-top: 2rem
+`;
+
+export const HeaderH1 = styled.h1`
+  width: 20%;
+  color: antiquewhite;
+  border-bottom: 1px solid antiquewhite;
+  margin-left: 1rem;
+`;
